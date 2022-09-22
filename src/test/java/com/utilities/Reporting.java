48095d10768ca299823f463dc16b352028cda7f0
Roman Yarmolenko <yarmolenko.r.a@gmail.com>
2022-09-22 17:30:13 +0200

    fixed screenshot issue

--- src/test/java/com/utilities/Reporting.java
+++ src/test/java/com/utilities/Reporting.java
@@ -1,10 +1,17 @@
 package com.utilities;
 
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Date;
 
 import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.utils.FileUtil;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
@@ -14,6 +21,8 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import static com.base.BaseClass.driver;
 
 
 public class Reporting extends TestListenerAdapter
@@ -26,9 +35,10 @@ public class Reporting extends TestListenerAdapter
 	{
 		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());//time stamp
 		String repName="Test-Report-"+timeStamp+".html"; // Report name and time stamp in html format
-		
+
 		htmlReporter=new ExtentHtmlReporter(System.getProperty("user.dir")+ "/test-output/"+repName);//specify location of the report
-		
+
+		htmlReporter.config().setAutoCreateRelativePathMedia(true);
 		htmlReporter.config().setDocumentTitle("Automation Report"); // Tile of report - given by the user
 		htmlReporter.config().setReportName("Functional Testing"); // name of the report - given by the user
 		htmlReporter.config().setTheme(Theme.DARK); //
@@ -44,6 +54,15 @@ public class Reporting extends TestListenerAdapter
 		extent.setSystemInfo("OS","Windows10");
 		extent.setSystemInfo("Browser name","Chrome,Firefox,IE");
 	}
+
+	public void captureScreen(WebDriver driver, String tname) throws IOException
+	{
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File target = new File(System.getProperty("user.dir") + "/Screenshots/" + tname + ".png");
+		FileUtils.copyFile(source, target);
+		System.out.println("Screenshot taken");
+	}
 	public void onTestSuccess (ITestResult result)
 	{
 
@@ -53,29 +72,45 @@ public class Reporting extends TestListenerAdapter
 		
 		}
 	
-	public void onTestFailure (ITestResult result)
-	{
-		test=extent.createTest(result.getName()); // create new entry in the report
+	public void onTestFailure (ITestResult result) {
 
-		test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getThrowable());
+		String failedTest = result.getName();
+		test=extent.createTest(failedTest); // create new entry in the report
 
-
-
-		String screenshotPath=System.getProperty("user.dir")+"\\Screenshots\\"+result.getName()+".png";
+		String screenshotPath=System.getProperty("user.dir")+ "//Screenshots" + failedTest + ".png";
 
 		try {
-			test.log(Status.FAIL, "Test Case FAILED IS " + result.getName()).
-					addScreenCaptureFromPath(screenshotPath);
-					// adding screenshot
+			captureScreen(driver, failedTest);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		test.error("Test Case: " + failedTest + " FAILED");
+		test.log(Status.FAIL, "TEST CASE FAILED TO " + result.getThrowable());
+
+
+		try {
+			test.log(Status.FAIL, "Screenshot").addScreenCaptureFromPath(screenshotPath, failedTest);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 
-		extent.flush();
+//		String screenshotPath=System.getProperty("user.dir") + "/Screenshots/" + result.getName() + ".png";
+//		try {
+//			test.addScreenCaptureFromPath(screenshotPath);// adding screenshot
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		// adding screenshot
+
+		}
 
 
-	}
+
+
+
+
 	public void onTestSkipped (ITestResult result)
 	{
 		test=extent.createTest(result.getName()); // create new entry in the report
